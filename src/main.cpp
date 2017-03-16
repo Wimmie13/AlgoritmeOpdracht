d976d9bab49c5f2acfb787b377a9e96ec6be429b
Willem van Dam <w.vandam@jvdict.nl>
2017-03-16 13:54:23 +0100

    Remove some useless code

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,5 @@ int main(int argc, char **argv)
 	JobShop jobShop(input.getAmountOfMachines());
 	input.generateJobs(jobShop);
 
-
-
-//	for (unsigned int i = 0; i < test.size(); i++)
-//	{
-//		for (unsigned int j = 0; j < test.at(i).size(); j++)
-//		{
-//			std::cout << test.at(i).at(j) << " ";
-//		}
-//		std::cout << std::endl;
-//	}
 	return 0;
 }
