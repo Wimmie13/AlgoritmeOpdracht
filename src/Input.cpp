cabb955ef170a49833d2b9a8782b542f70d59a32
Willem van Dam <w.vandam@jvdict.nl>
2017-03-16 22:57:05 +0100

    fixed argument

--- src/Input.cpp
+++ src/Input.cpp
@@ -16,20 +16,7 @@
 
 Input::Input(const std::string& filename)
 {
-	std::string testfile = "testFile.test";
-	std::string test = "6 6\n"
-			"2 1 0 3 1 6 3 7 5 3 4 6\n"
-			"1 8 2 5 4 10 5 10 0 10 3 4\n"
-			"2 5 3 4 5 8 0 9 1 1 4 7\n"
-			"1 5 0 5 2 5 3 3 4 8 5 9\n"
-			"2 9 1 3 4 5 5 4 0 3 3 1\n"
-			"1 3 3 3 5 9 0 10 4 4 2 1";
-	test = "2 3\n"
-			"0 30 1 30 2 10\n"
-			"0 60 1 15 2 10";
-	std::ofstream(testfile, std::ios::binary) << test;
-
-	std::ifstream input(testfile);
+	std::ifstream input(filename);
 	std::string line;
 	std::regex line_regex("[0-9]+");
 
